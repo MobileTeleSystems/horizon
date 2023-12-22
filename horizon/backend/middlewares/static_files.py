2fe8c431c623045412b2bc32ca3fff7a8c510f90
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2023-12-22 14:27:32 +0300

    [DOP-11121] Fix static files middleware is messing up with openapi_url

--- horizon/backend/middlewares/static_files.py
+++ horizon/backend/middlewares/static_files.py
@@ -10,7 +10,6 @@ from horizon.backend.settings.server.static_files import StaticFilesSettings
 def apply_static_files(app: FastAPI, settings: StaticFilesSettings) -> FastAPI:
     """Add static files serving middleware to the application."""
     if not settings.enabled:
-        app.openapi_url = None
         return app
 
     # https://fastapi.tiangolo.com/how-to/custom-docs-ui-assets/#serve-the-static-files

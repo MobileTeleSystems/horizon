410e968d0fc48f3100600846702136b6d4ff6504
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-02-14 15:01:44 +0300

    [DOP-3161] Fix documentation copyright

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.fspath(PROJECT_ROOT_DIR))
 # -- Project information -----------------------------------------------------
 
 project = "horizon"
-copyright = "2023-2024, DataOps.ETL"
+copyright = "2023-2024 MTS (Mobile Telesystems)"
 author = "DataOps.ETL"
 
 # The version info for the project you're documenting, acts as replacement for

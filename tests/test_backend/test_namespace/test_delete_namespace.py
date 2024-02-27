07a6dd4c6478422939e03066068a257f47fdfbdc
Maxim Liksakov <67663774+maxim-lixakov@users.noreply.github.com>
2024-02-27 17:45:40 +0300

    [DOP-13019] - restrict deletion namespaces with hwms (#25)
    * [DOP-13016] - add namespace_history table and its api methods
    
    * [DOP-13016] - add namespace history client docs
    
    * [DOP-13016] - drop 'is_delete' field for User
    
    * [DOP-13019] - restrict deletion namespaces with hwms

--- tests/test_backend/test_namespace/test_delete_namespace.py
+++ tests/test_backend/test_namespace/test_delete_namespace.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 import pytest
 from sqlalchemy import select
 
-from horizon.backend.db.models import Namespace, NamespaceHistory, User
+from horizon.backend.db.models import HWM, Namespace, NamespaceHistory, User
 
 if TYPE_CHECKING:
     from httpx import AsyncClient
@@ -89,3 +89,59 @@ async def test_delete_namespace(
     assert created_namespace_history.action == "Deleted"
     assert created_namespace_history.changed_by_user_id == user.id
     assert pre_delete_timestamp <= created_namespace_history.changed_at <= post_delete_timestamp
+
+
+async def test_delete_namespace_with_existing_hwm(
+    test_client: AsyncClient,
+    access_token: str,
+    user: User,
+    namespace: Namespace,
+    hwm: HWM,
+    async_session: AsyncSession,
+):
+    response = await test_client.delete(
+        f"v1/namespaces/{namespace.id}",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "error": {
+            "code": "bad_request",
+            "message": "Cannot delete namespace because it has related HWM records.",
+            "details": None,
+        },
+    }
+
+    query = select(Namespace).where(Namespace.id == namespace.id)
+    result = await async_session.execute(query)
+    namespace_record = result.scalars().first()
+    assert namespace_record is not None
+
+    query_hwm = select(HWM).where(HWM.namespace_id == namespace.id)
+    result_hwm = await async_session.execute(query_hwm)
+    hwm_records = result_hwm.scalars().all()
+    assert len(hwm_records) > 0
+
+    response = await test_client.delete(
+        f"v1/hwm/{hwm.id}",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == 204
+
+    response = await test_client.delete(
+        f"v1/namespaces/{namespace.id}",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    assert response.status_code == 204
+
+    query = select(Namespace).where(Namespace.id == namespace.id)
+    result = await async_session.execute(query)
+    namespace_record = result.scalars().first()
+    assert namespace_record is None
+
+    query_hwm = select(HWM).where(HWM.namespace_id == namespace.id)
+    result_hwm = await async_session.execute(query_hwm)
+    hwm_records = result_hwm.scalars().all()
+    assert len(hwm_records) == 0

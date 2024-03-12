bfe963d6a69daed51440bf10ac3a7a94549f8ec2
maxim-lixakov <mvliksako1@mts.ru>
2024-03-12 18:15:53 +0300

    [DOP-13013] - add tests for sync client

---
+++ tests/test_client_sync/test_permissions/test_client_sync_update_permsisions.py
@@ -0,0 +1,100 @@
+# SPDX-FileCopyrightText: 2023 MTS (Mobile Telesystems)
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import re
+
+import pytest
+import requests
+
+from horizon.backend.db.models import Namespace, NamespaceUserRole, User
+from horizon.client.sync import HorizonClientSync
+from horizon.commons.exceptions import EntityNotFoundError, PermissionDeniedError
+from horizon.commons.schemas.v1 import (
+    PermissionsResponseV1,
+    PermissionsUpdateRequestV1,
+    PermissionUpdateRequestItemV1,
+)
+
+pytestmark = [pytest.mark.client_sync, pytest.mark.client]
+
+
+@pytest.mark.parametrize(
+    "namespace_with_users",
+    [
+        [("new_user", NamespaceUserRole.MAINTAINER)],
+    ],
+    indirect=["namespace_with_users"],
+)
+def test_sync_client_update_namespace_permissions(
+    namespace: Namespace, user: User, sync_client: HorizonClientSync, namespace_with_users: None
+):
+    changes = PermissionsUpdateRequestV1(
+        permissions=[
+            PermissionUpdateRequestItemV1(username=user.username, role=NamespaceUserRole.DEVELOPER.name),
+            PermissionUpdateRequestItemV1(username="new_user", role=NamespaceUserRole.OWNER.name),
+        ]
+    )
+    response = sync_client.update_namespace_permissions(namespace.id, changes)
+
+    assert isinstance(response, PermissionsResponseV1)
+    assert any(
+        perm.username == user.username and perm.role == NamespaceUserRole.DEVELOPER.name
+        for perm in response.permissions
+    )
+    assert any(
+        perm.username == "new_user" and perm.role == NamespaceUserRole.OWNER.name for perm in response.permissions
+    )
+
+
+def test_sync_client_update_namespace_permissions_namespace_missing(
+    new_namespace: Namespace, sync_client: HorizonClientSync
+):
+    changes = PermissionsUpdateRequestV1(
+        permissions=[
+            PermissionUpdateRequestItemV1(username="someuser", role=NamespaceUserRole.DEVELOPER.name),
+        ]
+    )
+    with pytest.raises(
+        EntityNotFoundError,
+        match=re.escape(f"Namespace with id={new_namespace.id!r} not found"),
+    ) as exc_info:
+        sync_client.update_namespace_permissions(new_namespace.id, changes)
+
+    assert isinstance(exc_info.value.__cause__, requests.exceptions.HTTPError)
+    assert exc_info.value.__cause__.response.status_code == 404
+
+
+@pytest.mark.parametrize(
+    "namespace_with_users",
+    [
+        [
+            ("owner_user", NamespaceUserRole.OWNER),
+            ("user1", NamespaceUserRole.DEVELOPER),
+            ("user2", NamespaceUserRole.DEVELOPER),
+        ],
+    ],
+    indirect=["namespace_with_users"],
+)
+@pytest.mark.parametrize(
+    "invalid_changes",
+    [
+        PermissionsUpdateRequestV1(
+            permissions=[
+                PermissionUpdateRequestItemV1(username="user1", role="OWNER"),
+                PermissionUpdateRequestItemV1(username="user2", role="OWNER"),
+            ]
+        ),
+    ],
+)
+def test_sync_client_update_namespace_permissions_invalid(
+    namespace: Namespace,
+    invalid_changes: PermissionsUpdateRequestV1,
+    sync_client: HorizonClientSync,
+    namespace_with_users: None,
+):
+    with pytest.raises(requests.exceptions.HTTPError) as exc_info:
+        sync_client.update_namespace_permissions(namespace.id, invalid_changes)
+
+    assert exc_info.value.response.status_code == 400

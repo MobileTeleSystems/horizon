56235ffba039b704fd880b84480604e7a39d16eb
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-03-20 11:53:06 +0300

    [DOP-13779] Add robots.txt to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -122,6 +122,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_extra_path = ["robots.txt"]
 html_css_files = [
     "custom.css",
 ]

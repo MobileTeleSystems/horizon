f4de87046e55becf7ca545c08a811fa9028abe58
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-11-22 14:44:17 +0300

    [DOP-21634] Remove license headers from tests

--- tests/factories/user.py
+++ tests/factories/user.py
@@ -1,5 +1,3 @@
-# SPDX-FileCopyrightText: 2023 MTS PJSC
-# SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
 import secrets
--- tests/test_database/__init__.py
+++
@@ -1,2 +0,0 @@
-# SPDX-FileCopyrightText: 2023 MTS PJSC
-# SPDX-License-Identifier: Apache-2.0
